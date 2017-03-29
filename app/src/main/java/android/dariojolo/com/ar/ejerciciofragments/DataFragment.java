e4f685725cf4dc8f50df9fb4bc3ac61260ca1976
dariojolo <dariojolo@gmail.com>
2017-03-29 17:46:36 -0300

    Initial commit

---
+++ app/src/main/java/android/dariojolo/com/ar/ejerciciofragments/DataFragment.java
@@ -0,0 +1,65 @@
+package android.dariojolo.com.ar.ejerciciofragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DataFragment extends Fragment {
+
+
+    private EditText txtIngreso;
+    private Button  btnSend;
+    private DataListener dataListener;
+
+    public DataFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            dataListener = (DataListener) context;
+        }catch (Exception e){
+            throw new ClassCastException(context.toString() + " debe implementar DataListener");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_data,container, false);
+        // Logica para capturar los elementos de la UI
+        txtIngreso = (EditText)view.findViewById(R.id.txtIngreso);
+        btnSend    = (Button)view.findViewById(R.id.btnSend);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendText(txtIngreso.getText().toString());
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    private void sendText(String texto){
+        dataListener.sendData(texto);
+    }
+    public interface DataListener{
+        void sendData(String text);
+    }
+
+}
---
+++ app/src/main/java/android/dariojolo/com/ar/ejerciciofragments/DetailsFragment.java
@@ -0,0 +1,39 @@
+package android.dariojolo.com.ar.ejerciciofragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailsFragment extends Fragment {
+
+
+    private TextView details;
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+
+        details = (TextView)view.findViewById(R.id.txtDetails);
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+    public void renderizarTexto(String texto){
+        details.setText(texto);
+    }
+
+}
